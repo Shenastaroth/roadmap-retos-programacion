633ac40b7b216e6fbbc9caae78f3cf79dffed81e
Fernando <achingfernando20@gmail.com>
2026-06-21 13:38:06 -0500

    Ejercicio00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Shenastaroth.java
@@ -0,0 +1,36 @@
+public class Shenastaroth {
+    public static void main(String[] args) {
+        // URl del sitio oficial de java: https://www.java.com/es/
+
+        //Una linea
+         /*
+          Esto es un
+          comentario
+          en varias
+          lineas
+         */
+        
+
+          /**
+           * Esto es un comentario para documentacion para API o clases, metodos, etc.
+           * @author Shenastaroth
+           */
+
+          //Variable y Constante
+          String name = "Shenastaroth";
+          final String LAST_NAME = "Aching"; 
+
+          //Variables
+          byte num1 = 12;
+          int num2 = 1;
+          short num3 = 2;
+          Long num4 = 112L;
+          float decimal1 = 1.15f;
+          double decimal2 = 1.45;
+          boolean isTrue = true;
+          char letra = 'a';
+
+
+          System.out.println("Hola, Java");
+    }
+}
