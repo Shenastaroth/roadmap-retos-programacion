3f7c1a52921d81e9df5aabca41c06058d2a34da6
Fernando <achingfernando20@gmail.com>
2026-06-26 17:36:41 -0500

    Dirigiendo a la posicion correcta del archivo

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Shenastaroth.java
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Shenastaroth.java
@@ -1,36 +1,82 @@
-public class Shenastaroth {
+class Shenastaroth {
     public static void main(String[] args) {
-        // URl del sitio oficial de java: https://www.java.com/es/
+        /*
+            Operadores
+        */
 
-        //Una linea
-         /*
-          Esto es un
-          comentario
-          en varias
-          lineas
-         */
+        System.out.println("Suma : " + (5 + 7));    
+        System.out.println("Resta : " + (5 - 7));    
+        System.out.println("Multiplicacion : " + (5 * 7));    
+        System.out.println("Division : " + (5 / 7));
+        System.out.println("Modulo : " + (12 % 2));
         
-
-          /**
-           * Esto es un comentario para documentacion para API o clases, metodos, etc.
-           * @author Shenastaroth
-           */
-
-          //Variable y Constante
-          String name = "Shenastaroth";
-          final String LAST_NAME = "Aching"; 
-
-          //Variables
-          byte num1 = 12;
-          int num2 = 1;
-          short num3 = 2;
-          Long num4 = 112L;
-          float decimal1 = 1.15f;
-          double decimal2 = 1.45;
-          boolean isTrue = true;
-          char letra = 'a';
+        System.out.println();
 
 
-          System.out.println("Hola, Java");
+        /*
+            Operadores de Comparacion
+        */
+
+            System.out.println("Igualdad: 5 == 7 : " + (5 == 7));
+            System.out.println("Desigualdad: 5 != 7 : " + (5 != 7));
+            System.out.println("Menor que: 5 < 7 : " + (5 < 7));
+            System.out.println("Mayor que: 5 > 7 : " + (5 > 7));
+            System.out.println("Menor o igual: 5 <= 7 : " + (5 <= 7));
+            System.out.println("Mayor o igual: 5 >= 7 : " + (5 >= 7));
+
+        /*
+            Operadores Logicos
+         */
+
+            System.out.println("AND &&: true && true : " + (12 < 22 && 2 < 8));
+            System.out.println("OR || : " + (12 < 22 || 2 > 8));
+            System.out.println("NOT: !true : " + (!true));
+
+        /*
+         Operadoes de asignacion
+        */
+
+         int number = 12;
+         System.out.println(number);
+         number += 1;//suma y asignacion
+         System.out.println(number);
+         number -= 1;//resta y asignacion
+         System.out.println(number);
+         number *= 2; //multiplicacion y asigancion
+         System.out.println(number);
+         number /= 2; //division y asignacion
+         System.out.println(number);
+         number %= 2; // modulo y asigancion
+
+         /*
+          Operadores de identidad
+         */
+
+          int myNumber = number;
+          System.out.println("number is myNumber: " + (number == myNumber));
+
+          System.out.println("number is not myNumber: " + (number != myNumber));
+
+          /*
+           Operadores de bit
+          */
+
+          /*
+          AND :  Devuelve 1 en cada posiscion donde amos bits son 1
+          OR : Devuelve 1 si al menos uno de los bits es 1
+          XOR : devuelve 1 si los bits son diferentes
+          NOT : Operador unario que invierte todos los bits(cambia 0 por 1 y viciversa)
+          */
+
+           int a = 10;
+           int b = 5;
+           System.out.println("AND : 10 & 5 = " + (a & b));
+           System.out.println("OR : 10 || 5 = " + (a & b));
+           System.out.println("XOR : 10 ^ 5 = " + (a ^ b));
+           System.out.println("NOT : -10 = " + (-a));
+           System.out.println("Desplazamiento a la derecha : 10 >> 2 = " + (a >> 2));
+           System.out.println("Desplazamiento a la izquierda : 10 << 2 = " + (a << 2));
+           
+
     }
 }
